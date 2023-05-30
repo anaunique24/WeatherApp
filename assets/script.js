1aa44985fb1989df07b036e23a62662d5cfab25c
Ana Grisham <anaunique@icloud.com>
2023-05-30 16:19:10 -0700

    created global variables

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var searchResultsEl = document.querySelector('#search-results');
+var userSearchEl = document.querySelector('#q');
+var searchFormEl = document.querySelector('#search-form');
+var submitBtn = document.querySelector('#btn btn-primary');
+var currentDate = dayjs().format('MM/DD/YYYY');
